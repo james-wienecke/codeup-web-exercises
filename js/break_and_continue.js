22fe366fffb67457c7347c384bdd09304a5fd624
James Wienecke <james.a.wienecke@gmail.com>
2021-10-18 17:17:56 -0500

    small changes and additional documentation added

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -2,7 +2,7 @@
     console.log("Break and Continue exercise:")
     console.log("[Odd numbers with user input]")
     // prompt user for odd number between 1 and 50
-    // using arrow functions with anonymous functions
+    // using arrow functions & anonymous functions for practice. not the best place for them but...... fun
     const isOdd = (num) => num % 2 !== 0; // implicit return
     const isInRange = (min, max, num) => { return (num >= min) && (num <= max); }
     let userNum = 0;
@@ -12,16 +12,16 @@
             break;
         }
     }
-    console.log(`Number to skip is`, userNum);
-    for (let i = 0; i <= 50; i++) {
+    console.log("Number to skip is", userNum);
+    for (let i = 1; i <= 50; i += 2) {  // starting on 1 and increment by 2 to only check odd numbers
         if (isOdd(i)) {
             if (i === userNum) {
-                console.log(`Yikes! Skipping user's number:`, userNum);
+                console.log("Yikes! Skipping user's number:", userNum);
             } else {
-                console.log(`Here's an odd number:`, i);
+                console.log("Here's an odd number:", i);
             }
         } else {
-            continue;
+            continue; // this should literally NEVER run but it's here for the sake of exercise completion
         }
 }
 }());
--- js/while.js
+++ js/while.js
@@ -37,20 +37,26 @@
      */
     // This is how you get a random number between 50 and 100
     let allCones = Math.floor(Math.random() * 50) + 50;
+    console.log(`I have to sell ${allCones} ice cream cones before I can go back home and pet my dogs and cats...`);
     // This expression will generate a random number between 1 and 5
     //Math.floor(Math.random() * 5) + 1;
+    // iceCreamReport is main logical body of exercise part
+    function iceCreamReport(all, delta) {
+        // this little function will handle silly english plural cases if passed the quantity of objects being described
+        const plural = (d) => { return (d != 1) ? `s` : `` };
+        // maybe some people don't like them but I've become addicted to template literals.
+        // I actually had to remove some from other parts of today's exercise to increase output info density
+        if (all < 0) {
+            return `Cannot sell ${delta} cone${plural(delta)}. I could only sell ${all + delta} cone${plural(all + delta)}.`;
+        } else {
+            return `Yay! ${delta} cone${plural(delta)} sold. ${all} cone${plural(all)} left to go!`;
+        }
+    }
+    // main loop happens in this block. continue selling ice cream cones until there are no more cones to sell! yum
     do {
         let boughtCones = Math.floor(Math.random() * 5) + 1;
         allCones -= boughtCones;
         console.log(iceCreamReport(allCones, boughtCones));
     } while (allCones > 0);
-    console.log("Time to go home!")
-    function iceCreamReport(all, delta) {
-        const plural = (d) => { return (d != 1) ? `s` : `` };
-        if (all < 0) {
-            return `Cannot sell ${delta} cone${plural(delta)}. I could only sell ${all + delta} cone${plural(all + delta)}...`;
-        } else {
-            return `Yay! ${delta} cone${plural(delta)} sold. ${all} cone${plural(all)} left to go!`;
-        }
-    }
+    console.log("Time to go home!");
 }());
