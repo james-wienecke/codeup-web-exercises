f45a3f16458ac0bcc159f3f1ce10efb012e96c1e
James Wienecke <james.a.wienecke@gmail.com>
2021-11-29 10:59:40 -0600

    Completed essential methods lecture.

--- js/essential-methods.js
+++ js/essential-methods.js
@@ -1,7 +1,27 @@
 $(document).ready(function () {
     let heading = $('#heading').html();
-    // alert(heading)
+    // alert(heading);
     let cardContents = $('.card').html();
-    // alert(cardContents)
 
+    // jQuery will select the first element if there are multiple
+    // alert(cardContents);
+
+    $('.card').html("<h2>Hello from javascript</h2>");
+
+    // we can chain methods
+    let box = $('#box');
+    box.css('background-color', 'cyan')
+        .css('border', '8px solid cadetblue')
+        .css({ // we can also pass an object
+            'background-color': 'red',
+            'border': '8px solid maroon'
+        });
+    // alert(box.css('background'));
+
+    // jquery works sequentially like this
+    $('h2').addClass('highlighted');
+
+    $('.highlighted').click(function () {
+        $(this).removeClass('highlighted');
+    })
 });
