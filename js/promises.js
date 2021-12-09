11381dbb994340313c1ad5314977a8153e3dfe26
James Wienecke <james.a.wienecke@gmail.com>
2021-12-09 13:57:44 -0600

    Continuing to work building out the async wait section of the page. Forms and events are working but the ability to wait with more than one input at once is not functional.

--- js/promises.js
+++ js/promises.js
@@ -42,4 +42,80 @@ $(document).ready(() => {
         .then(time => logTime(time))
         .catch(time => logTimeFail(time));
 
+
+
+
+    /*  <section id="wait-input-container" class="">
+            <div class="input-group">
+                <input type="text" class="form-control wait-input" id="wait-time-input-0" placeholder="Enter a time to wait in seconds"
+                       aria-label="specify time to wait" aria-describedby="time-to-wait">
+                <div class="input-group-append">
+                    <button class="btn btn-primary" type="button" id="wait-time-submit-0">Start waiting!</button>
+                </div>
+            </div>
+        </section>
+     */
+    const createWaitInputGroup = (index) => {
+        const $label = $(document.createElement('p'))
+            .addClass('text-secondary text-monospace pr-2 my-auto')
+            .attr('id', `wait-input-label-${index}`)
+            .text(`Wait #${index + 1}`);
+
+        const $input = $(document.createElement('input'))
+            .addClass('form-control wait-input')
+            .attr('id', `wait-time-input-${index}`)
+            .attr('type', 'text')
+            .attr('placeholder', 'Enter a time to wait in seconds')
+            .attr('aria-label', 'specify time to wait')
+            .attr('aria-describedby', 'time-to-wait');
+
+        const $button = $(document.createElement('div'))
+            .addClass('input-group-append wait-button')
+            .append($(document.createElement('button'))
+                .addClass('btn btn-primary')
+                .attr('id', `wait-time-submit-${index}`)
+                .attr('type', 'button')
+                .text('Start waiting!')
+                .data('index', index));
+
+        return $(document.createElement('div'))
+            .addClass('input-group wait-input-group align-content-center py-1')
+            .append($label, $input, $button);
+    }
+    $('#wait-input-container').append(createWaitInputGroup(0))
+    $('#wait-input-container').append(createWaitInputGroup(1))
+
+    /*
+            <div class="wait-display">
+                <h5>Waiting for <span class="wait-label"></span></h5>
+                <p class="wait-result"></p>
+            </div>
+     */
+    const createWaitResultsElem = index => {
+        const $label = $(document.createElement('h5'))
+            .addClass('text-secondary text-monospace py-2 my-auto mr-2')
+            .attr('id', `wait-result-label-${index}`)
+            .text(`Wait #${index + 1}`)
+
+        const $result = $(document.createElement('p'))
+            .addClass('text-primary py-2 my-auto ml-2')
+            .attr('id', `wait-result-${index}`)
+            .text('');
+
+        return $(document.createElement('div'))
+            .addClass('input-group-append wait-display')
+            .append($label, $result);
+    }
+
+    $('#wait-results-container').append(createWaitResultsElem(0))
+    $('#wait-results-container').append(createWaitResultsElem(1))
+
+    $('.wait-button').on('click', function (e) {
+        e.preventDefault();
+        const index = $(this).children('button').data('index');
+        const input = parseFloat($(this).parent().children('input').val());
+        wait(input * 1000)
+            .then(time => $(`#wait-result-${index}`).text(`Waited for ${time/1000} seconds`))
+            .catch(time => logTimeFail(time));
+    });
 });
