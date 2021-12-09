977a853fc7afd39c4d51b8f02ef50f08a959397a
James Wienecke <james.a.wienecke@gmail.com>
2021-12-09 12:10:17 -0600

    Solved bonus. Passing arguments through then() is pretty cool.

--- js/promises.js
+++ js/promises.js
@@ -7,7 +7,31 @@ $(document).ready(() => {
             .then((data) => {
                 let pushes = data.filter(event => event.type === 'PushEvent');
                 console.log(pushes[0].created_at);
-            });
+            })
+            .catch(() => console.log('Github API request failed. Check your input.'));
     }
     getUserEvent('james-wienecke');
+
+    const wait = (time) => {
+        const myPromise = new Promise((resolve, reject) => {
+                if (typeof time !== 'number') reject(time);
+                let timeout = setTimeout(() => {
+                    resolve(time);
+                }, time);
+            })
+        return myPromise;
+        };
+
+    const logTime = (time) => `resolved in ${time}ms`;
+    const logTimeFail = (time) => `Bad input? arg: ${time}, arg type: ${typeof time}`;
+
+    wait(2000)
+        .then((time) => console.log(logTime(time)))
+        .catch((time) => console.error(logTimeFail(time)));
+    wait(1000)
+        .then((time) => console.log(logTime(time)))
+        .catch((time) => console.error(logTimeFail(time)));
+    wait('cat')
+        .then((time) => console.log(logTime(time)))
+        .catch((time) => console.error(logTimeFail(time)));
 });
